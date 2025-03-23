82121acafc76c384c2ad68de21f5bbcb2091f309
kopertt <dimkanebroev@mail.ru>
2025-03-23 13:58:03 +0400

    добавил отображение 3-х ласт

--- frontend/digital-wild/src/components/EventsSection.jsx
+++ frontend/digital-wild/src/components/EventsSection.jsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './EventsSection.css';
 
-const eventsData = [
-  {
-    image: "../images/people.png", // Используйте относительный путь
-    title: "Мероприятие по уборке листьев",
-    description: "Давайте убирать листья вместе! Вместе - мы сила! Тут должно быть больше текста, но у меня кончилась фантазия."
-  },
-  {
-    image: "../images/people.png", // Используйте относительный путь
-    title: "Разгульная масленица",
-    description: "Время попрощаться с зимой! Тут должно быть больше текста, но у меня кончилась фантазия."
-  },
-  {
-    image: "../images/people.png", // Используйте относительный путь
-    title: "Мероприятие по уборке листьев",
-    description: "Давайте убирать листья вместе! Вместе - мы сила! Тут должно быть больше текста, но у меня кончилась фантазия."
-  }
-];
-
 const EventsSection = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    // Получаем последние 3 мероприятия
+    fetch('http://localhost:3000/api/events?page=1&limit=3')
+      .then(response => response.json())
+      .then(data => setEvents(data.events))
+      .catch(error => console.error('Ошибка загрузки мероприятий:', error));
+  }, []);
+
   return (
     <section className="events-section">
       <h2 className="events-title">МЕРОПРИЯТИЯ</h2>
       <div className="events">
-        {eventsData.map((event, index) => (
-          <div key={index} className="events-item">
+        {events.map(event => (
+          <div key={event.id} className="events-item">
             <div className="event-image-container">
-              <img 
-                src={event.image} 
-                alt={event.title} 
-                className="event-image"
-              />
+            <img 
+              src={`http://localhost:3000${event.image}`} 
+              alt={event.title} 
+              className="event-image"
+            />
             </div>
             <div className="events-details">
               <h3 className="event-title">{event.title}</h3>
-              <p className="events-description">{event.description}</p>
+              <p className="events-description">{event.content}</p>
             </div>
           </div>
         ))}
